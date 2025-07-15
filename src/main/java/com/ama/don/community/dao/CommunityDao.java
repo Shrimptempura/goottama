c74b02f3a1a9776f02230f6f756258723d1ef493
EUM11327 <gustjd11327@gmail.com>
2025-07-15 18:18:16 +0900

    review

---
+++ src/main/java/com/ama/don/community/dao/CommunityDao.java
@@ -0,0 +1,8 @@
+package com.ama.don.community.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommunityDao {
+
+}
--- src/main/java/com/ama/don/community/dto/CommunityDto.java
+++ src/main/java/com/ama/don/community/dto/CommunityDto.java
@@ -7,6 +7,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class Dto {
+public class CommunityDto {
 
 }
