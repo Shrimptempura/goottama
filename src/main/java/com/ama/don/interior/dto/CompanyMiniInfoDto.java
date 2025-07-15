a3c881d856869d857b11c4deb26e45ed7aa47210
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-15 10:57:37 +0900

    refator: private add

--- src/main/java/com/ama/don/interior/dto/CompanyMiniInfoDto.java
+++ src/main/java/com/ama/don/interior/dto/CompanyMiniInfoDto.java
@@ -12,26 +12,26 @@ import lombok.Setter;
 // 업체 좌측 미니 박스에 담길 정보
 public class CompanyMiniInfoDto {
 
-    Long companyDetailId;       // 업체상세 아이디
-    String companyName;         // 업체 이름
-    Boolean companyFollow;      // 팔로우 여부
-    Integer companyRate;        // 업체 평점(아직 평가하지 않음 -> Integer)
+    private Long companyDetailId;       // 업체상세 아이디
+    private String companyName;         // 업체 이름
+    private Boolean companyFollow;      // 팔로우 여부
+    private Integer companyRate;        // 업체 평점(아직 평가하지 않음 -> Integer)
 
     /**
      * 업체 AS
      */
-    String companyAs;
+    private String companyAs;
 
     /**
      * 업체 소개말
      */
-    String companyLetter;
-    String companyAddr;         // 업체 주소
+    private String companyLetter;
+    private String companyAddr;         // 업체 주소
 
     /**
      * 업체 분야
      */
-    String companyField;
-    String companyLicense;      // 업체 면허
-    String companyCareer;       // 업체 경력[사용 확인]
+    private String companyField;
+    private String companyLicense;      // 업체 면허
+    private String companyCareer;       // 업체 경력[사용 확인]
 }
