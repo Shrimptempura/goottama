be15407a845ab913a7298cea7c9cf5b9fb46e7af
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-15 10:53:14 +0900

    refactor: type 및 ui 구성에 맞게 수정

--- src/main/java/com/ama/don/interior/dto/CompanyMiniInfoDto.java
+++ src/main/java/com/ama/don/interior/dto/CompanyMiniInfoDto.java
@@ -12,25 +12,26 @@ import lombok.Setter;
 // 업체 좌측 미니 박스에 담길 정보
 public class CompanyMiniInfoDto {
 
-    Long company_detail_id;     // 업체상세 아이디
-    String company_addr;        // 업체 주소
-
-    /**
-     * 업체 분야
-     */
-    String company_field;
-    String company_license;     // 업체 면허
+    Long companyDetailId;       // 업체상세 아이디
+    String companyName;         // 업체 이름
+    Boolean companyFollow;      // 팔로우 여부
+    Integer companyRate;        // 업체 평점(아직 평가하지 않음 -> Integer)
 
     /**
      * 업체 AS
      */
-    String company_as;
-    String company_career;      // 업체 경력
-    String company_name;        // 업체 이름
-    int company_rate;           // 업체 평점
+    String companyAs;
 
     /**
      * 업체 소개말
      */
-    String company_letter;
+    String companyLetter;
+    String companyAddr;         // 업체 주소
+
+    /**
+     * 업체 분야
+     */
+    String companyField;
+    String companyLicense;      // 업체 면허
+    String companyCareer;       // 업체 경력[사용 확인]
 }
