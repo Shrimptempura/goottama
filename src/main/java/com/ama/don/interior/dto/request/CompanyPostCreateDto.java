c1661c600f977a48afab629d1f83d0e0bc76b095
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-17 17:15:13 +0900

    refactor(CompanyPostCreateDto): 작성의 기준으로 쓸모없는 필드값 제거 및 수정

--- src/main/java/com/ama/don/interior/dto/request/CompanyPostCreateDto.java
+++ src/main/java/com/ama/don/interior/dto/request/CompanyPostCreateDto.java
@@ -8,30 +8,23 @@ import lombok.Setter;
 import java.sql.Timestamp;
 import java.util.List;
 
+// 업체가 작성하는 게시글 dto
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-// 업체가 작성한 포스터
 public class CompanyPostCreateDto {
 
-    private Long companyPostId;      // 업체 포스트(게시글) 아이디
-    private Long companyId;             // 업체 아이디
-    private String companyIntro;        // 업체 소개말
-    private Boolean isFollowed;      // 업체 팔로우
+    private Long postId;            // 업체 포스트(게시글) 아이디
+    private Long companyId;         // 업체 아이디
 
     private List<String> companyImgList;    // 업체가 올린 사진
+    private String postTitle;       // 게시글 제목
 
-    /**
-     * sql 추가 부분, 인테리어 설명
-     */
-    private String spaceType;       // 공간 종류
+    private String spaceType;       // 공간 종류(아파트, 주택)
     private String areaInPyeong;      // 평수
-    private String location;        // 지역[location table 확인]
-    private String style;           // 스타일
-    private String constructionDetail;   // 세부 공사
+    private String style;           // 스타일(내추럴, 모던..)
+    private String constructionDetail;   // 세부 공사(주방리모델링, 도배시공..)
 
-    private Timestamp postDate;         // 작성시간
-    private int likeCount;      // 좋아요 수
-    private int viewCount;      // 게시판 조회수
+    private String postContent;     // 게시글 내용
 }
