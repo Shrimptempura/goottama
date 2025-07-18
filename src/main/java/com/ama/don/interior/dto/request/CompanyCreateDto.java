18574bd10907c5b96610700bf2af3000fc6a9ca8
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-18 16:47:51 +0900

    feat(companyRequestDto): create, update에  필요한 dto 초안 작성

---
+++ src/main/java/com/ama/don/interior/dto/request/CompanyCreateDto.java
@@ -0,0 +1,17 @@
+package com.ama.don.interior.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// 업체에 대한 모든 정보 dto
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CompanyCreateDto {
+
+
+
+}
