da322d456fb2c09b953b0d652944da62a71735e8
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-22 12:40:05 +0900

    feat(insertLocation): 위치 주소 생성 테이블 mapper

--- src/main/java/com/ama/don/interior/dto/request/CompanyCreateLocationDto.java
+++ src/main/java/com/ama/don/interior/dto/request/CompanyCreateLocationDto.java
@@ -1,7 +1,6 @@
 package com.ama.don.interior.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,15 +19,9 @@ public class CompanyCreateLocationDto {
     @NotBlank
     private String locationAddr;    // 위치 주소(한글)
 
-    @NotBlank
     private String locationLat;     // 위치 위도
-
-    @NotBlank
     private String locationLng;     // 위치 경도
-
-    @NotBlank
     private String locationAc;      // 위치 코드
 
-    @NotBlank
     private String locationLimit;   // 위치 제한
 }
