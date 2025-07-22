7427e095f416e6bb5a02d37dea12aefbdf8ceb55
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-22 14:38:22 +0900

    test(insertCompanyy): company 테이블 생성 테스트

--- src/test/java/com/ama/don/interior/dao/CompanyDaoTest.java
+++ src/test/java/com/ama/don/interior/dao/CompanyDaoTest.java
@@ -2,6 +2,10 @@ package com.ama.don.interior.dao;
 
 import com.ama.don.interior.dto.request.CompanyCreateDto;
 import com.ama.don.interior.dto.request.CompanyCreateLocationDto;
+import com.ama.don.interior.dto.request.CompanyInsertDto;
+import com.ama.don.member.dao.JoinDao;
+import com.ama.don.member.dto.JoinformDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,8 +21,66 @@ class CompanyDaoTest {
     @Autowired
     CompanyDao companyDao;
 
+    @DisplayName("company_detail 테이블 생성 테스트")
     @Test
     void insertCompanyDetail() {
+        CompanyCreateDto detail = createTestCompanyDetail();
+        companyDao.insertCompanyDetail(detail);
+
+        assertThat(detail.getCompanyDetailId()).isNotNull();
+    }
+
+    @DisplayName("location 테이블에서 주소만 확인")
+    @Test
+    void insertLocation() {
+        CompanyCreateLocationDto location = createTestLocation();
+
+        companyDao.insertLocation(location);
+
+        assertThat(location.getLocationId()).isNotNull();
+    }
+
+    @DisplayName("여러 테이블에서 fk를 받아 생성한 company 테이블")
+    @Test
+    void insertCompany() {
+        JoinformDto user = createTestUser();
+        CompanyCreateDto detail = createTestCompanyDetail();
+        CompanyCreateLocationDto location = createTestLocation();
+
+        CompanyInsertDto dto = new CompanyInsertDto();
+        dto.setUserId(user.getUserId());
+        dto.setCompanyDetailId(detail.getCompanyDetailId());
+        dto.setLocationId(location.getLocationId());
+        dto.setCompanyImg("images/interior/test.img");
+
+        companyDao.insertCompany(dto);
+
+        assertThat(dto.getCompanyId()).isNotNull();
+    }
+
+
+
+    private JoinformDto createTestUser() {
+        JoinformDto dto = new JoinformDto();
+        dto.setLoginId("테스트아이디");
+        dto.setPw("abcdefghi!@");
+        dto.setPw2("abcdefghi!@");
+        dto.setName("홍길동");
+        dto.setNickname("테스트닉네임");
+        dto.setGender(JoinformDto.Gender.M);
+        dto.setBirth("1999-09-09");
+        dto.setTel("010-1234-5678");
+        dto.setZipcode("12345");
+        dto.setAddr("서울특별시 구로구");
+        dto.setEmailId("abcdefg");
+        dto.setEmailDomain("naver.com");
+
+        companyDao.insertUser(dto);
+
+        return dto;
+    }
+
+    private CompanyCreateDto createTestCompanyDetail() {
         CompanyCreateDto dto = new CompanyCreateDto();
         dto.setCompanyName("업체이름");
         dto.setCompanyAddr("업체주소");
@@ -30,16 +92,16 @@ class CompanyDaoTest {
 
         companyDao.insertCompanyDetail(dto);
 
-        assertThat(dto.getCompanyDetailId()).isNotNull();
+        return dto;
     }
 
-    @Test
-    void insertLocation() {
+    private CompanyCreateLocationDto createTestLocation() {
         CompanyCreateLocationDto dto = new CompanyCreateLocationDto();
         dto.setLocationAddr("서울특별시 구로구");
 
         companyDao.insertLocation(dto);
 
-        assertThat(dto.getLocationId()).isNotNull();
+        return dto;
     }
+
 }
