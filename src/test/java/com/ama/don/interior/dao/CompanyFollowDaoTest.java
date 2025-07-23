103f9b7a3d322b8c184102a5633e886276b90831
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-23 16:19:26 +0900

    test(CompanyFollowDaoTest): 코드 내 회원가입후 업체 등록 중복 제거

--- src/test/java/com/ama/don/interior/dao/CompanyFollowDaoTest.java
+++ src/test/java/com/ama/don/interior/dao/CompanyFollowDaoTest.java
@@ -23,16 +23,7 @@ class CompanyFollowDaoTest extends AbstractCompanyTestSupport {
     @DisplayName("회원이 업체 팔로우, 테이블 생성")
     @Test
     void insertFollowCompany() {
-        JoinformDto user = createTestUser();
-        CompanyCreateDto detail = createTestCompanyDetail();
-        CompanyCreateLocationDto location = createTestLocation();
-
-        CompanyInsertDto dto = new CompanyInsertDto();
-        dto.setUserId(user.getUserId());
-        dto.setCompanyDetailId(detail.getCompanyDetailId());
-        dto.setLocationId(location.getLocationId());
-
-        companyDao.insertCompany(dto);
+        CompanyInsertDto dto = createTestCompany();
         Long companyId = dto.getCompanyId();
 
         JoinformDto otherUser = createTestUser("otherUser111");
@@ -49,27 +40,8 @@ class CompanyFollowDaoTest extends AbstractCompanyTestSupport {
     @Test
     void deleteFollowCompany() {
         // 업체 2개 생성
-        JoinformDto user = createTestUser();
-        CompanyCreateDto detail = createTestCompanyDetail();
-        CompanyCreateLocationDto location = createTestLocation();
-
-        CompanyInsertDto dto = new CompanyInsertDto();
-        dto.setUserId(user.getUserId());
-        dto.setCompanyDetailId(detail.getCompanyDetailId());
-        dto.setLocationId(location.getLocationId());
-
-        companyDao.insertCompany(dto);
-
-        JoinformDto otherUser = createTestUser("otherUser111");
-        CompanyCreateDto otherDetail = createTestCompanyDetail("otherCompany");
-        CompanyCreateLocationDto otherLocation = createTestLocation();
-
-        CompanyInsertDto otherDto = new CompanyInsertDto();
-        otherDto.setUserId(otherUser.getUserId());
-        otherDto.setCompanyDetailId(otherDetail.getCompanyDetailId());
-        otherDto.setLocationId(otherLocation.getLocationId());
-
-        companyDao.insertCompany(otherDto);
+        CompanyInsertDto dto = createTestCompany();
+        CompanyInsertDto otherDto = createTestCompany("otherUser111", "otherCompany");
 
         // 팔로우 테스트 회원
         JoinformDto thirdUser = createTestUser("thirdUser111");
@@ -78,13 +50,11 @@ class CompanyFollowDaoTest extends AbstractCompanyTestSupport {
         CompanyFollowDto followDto = new CompanyFollowDto();
         followDto.setUserId(thirdUser.getUserId());
         followDto.setCompanyId(dto.getCompanyId());
-
         companyFollowDao.insertFollowCompany(followDto);
 
         CompanyFollowDto secondFollowDto = new CompanyFollowDto();
         secondFollowDto.setUserId(thirdUser.getUserId());
         secondFollowDto.setCompanyId(otherDto.getCompanyId());
-
         companyFollowDao.insertFollowCompany(secondFollowDto);
 
         // 2개의 업체 팔로우 확인
@@ -105,27 +75,17 @@ class CompanyFollowDaoTest extends AbstractCompanyTestSupport {
     @DisplayName("로그인 한 회원이 업체를 팔로우 한 경우")
     @Test
     void isFollowedCompanyReturnTrueWhenFollowed() {
-        JoinformDto user = createTestUser();
-        CompanyCreateDto detail = createTestCompanyDetail();
-        CompanyCreateLocationDto location = createTestLocation();
-
-        CompanyInsertDto dto = new CompanyInsertDto();
-        dto.setUserId(user.getUserId());
-        dto.setCompanyDetailId(detail.getCompanyDetailId());
-        dto.setLocationId(location.getLocationId());
-        dto.setCompanyImg("images/interior/test.img");
-
-        companyDao.insertCompany(dto);
+        CompanyInsertDto dto = createTestCompany();
         Long companyId = dto.getCompanyId();
 
         // 회원과 업체는 팔로우 상태
         CompanyFollowDto followDto = new CompanyFollowDto();
-        followDto.setUserId(user.getUserId());
+        followDto.setUserId(dto.getUserId());
         followDto.setCompanyId(companyId);
 
         companyFollowDao.insertFollowCompany(followDto);
 
-        Boolean result = companyFollowDao.isFollowedCompany(companyId, user.getUserId());
+        Boolean result = companyFollowDao.isFollowedCompany(companyId, dto.getUserId());
 
         assertThat(result).isTrue();
     }
@@ -133,17 +93,7 @@ class CompanyFollowDaoTest extends AbstractCompanyTestSupport {
     @DisplayName("로그인 한 회원이 업체를 팔로우 하지 않은 경우")
     @Test
     void isFollowedCompanyReturnFalseWhenNotFollowed() {
-        JoinformDto user = createTestUser();
-        CompanyCreateDto detail = createTestCompanyDetail();
-        CompanyCreateLocationDto location = createTestLocation();
-
-        CompanyInsertDto dto = new CompanyInsertDto();
-        dto.setUserId(user.getUserId());
-        dto.setCompanyDetailId(detail.getCompanyDetailId());
-        dto.setLocationId(location.getLocationId());
-        dto.setCompanyImg("images/interior/test.img");
-
-        companyDao.insertCompany(dto);
+        CompanyInsertDto dto = createTestCompany();
         Long companyId = dto.getCompanyId();
 
         // 팔로우하지 않은 다른 유저
