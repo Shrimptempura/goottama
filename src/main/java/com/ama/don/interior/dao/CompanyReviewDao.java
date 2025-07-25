9a4539b9222ef374a44fed11eac83e7e656ad03c
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-25 16:09:25 +0900

    refactor: CompanyDao와 CompanyReviewDao의 책임 분리

--- src/main/java/com/ama/don/interior/dao/CompanyReviewDao.java
+++ src/main/java/com/ama/don/interior/dao/CompanyReviewDao.java
@@ -7,10 +7,17 @@ import com.ama.don.interior.dto.response.CompanyReviewDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 // 사용자가 업체에 쓰는 리뷰 dao
 @Mapper
 public interface CompanyReviewDao {
+
+    // 리뷰 작성
+    int insertCompanyReview(CompanyReviewCreateDto dto);
+
+    // 리뷰 점수 계산 테이블 생성
+    int insertScoreTable(CompanyReviewCreateDto dto);
 
     /**
      * 홈에서 보는 업체에 대한 리뷰 목록 뷰(최신 순)
@@ -22,11 +29,14 @@ public interface CompanyReviewDao {
      */
     List<CompanyReviewDto> listByCompanyId(Long companyId);
 
-    // 리뷰 작성
-    void create(CompanyReviewCreateDto dto);
-
     // 리뷰 상세보기
     CompanyReviewDto detail(Long reviewId);
+
+    // 리뷰 평균 점수 계산 업데이트
+    int updateScoreAvg(CompanyReviewCreateDto dto);
+
+    // 업체 별점 평균, 단순 값 추출 추후 정렬에 이용 가능성
+    Optional<Double> getAvgScore(Long companyId);
 
     // 리뷰 삭제
     void delete(Long reviewId);
