e7b68695d4538a2cb04a88c98445820db908fdeb
Cha-Hyunju <hj4530hj271@naver.com>
2025-07-28 12:23:36 +0900

    feat:jsp추가

--- src/main/java/com/ama/don/member/controller/MemberController.java
+++ src/main/java/com/ama/don/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.ama.don.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,8 +26,57 @@ public class MemberController {
 		
 		if (!success) {
 			return "member/resetPw_view";
-		}
-		
+		}		
 		return "redirect:/login_view";
+	}
+	
+	@GetMapping("/mypage/myProfile")
+	public String memberProfile() {
+		return "member/mypage/myProfile";
+	}
+	
+	@GetMapping("/mypage/myOrderList")
+	public String memberOrderList() {
+		return "member/mypage/myOrderList";
+	}
+	
+	@GetMapping("/mypage/myScrapbook")
+	public String memberScrapbook() {
+		return "member/mypage/myScrapbook";
+	}
+	
+	@GetMapping("/mypage/myInquiry")
+	public String myInquiry() {
+		return "member/mypage/myInquiry";
+	}
+	
+	@GetMapping("/mypage/myReview")
+	public String myReview() {
+		return "member/mypage/myReview";
+	}
+	
+	@GetMapping("/mypage/myFeed")
+	public String myFeed() {
+		return "member/mypage/myFeed";
+	}
+	
+	@GetMapping("/mypage/myComment")
+	public String myComment() {
+		return "member/mypage/myComment";
+	}
+	
+	@GetMapping("/mypage/editProfile")
+	public String editProfile() {
+		return "member/mypage/editProfile";
+	}
+	
+	@GetMapping("/mypage/editPassword")
+	public String editPassword() {
+		return "member/mypage/editPassword";
+	}
+	
+	@GetMapping("/mypage/customerCenter")
+	public String customerCenter() {
+		return "member/mypage/customerCenter";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/member/mypage/myComment.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<a href="/mypage/myProfile"> 프로필</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mypage/myOrderList">나의쇼핑</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mypage/myReview"> <strong>나의활동</strong> </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mypage/editProfile">설정</a> <br />
+
+<a href="/mypage/myReview">내가남긴리뷰</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mypage/myFeed">나의피드</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mypage/myComment"><strong>내가남긴댓글</strong></a>
+
+
+</body>
+</html>
