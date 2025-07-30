2506580381754a795623300c6a3b892aa6676749
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-30 15:07:50 +0900

    feat(resetScoresIfOne): 리뷰가 만약 1개면 삭제시 0으로 대체

--- src/main/java/com/ama/don/interior/dao/CompanyReviewDao.java
+++ src/main/java/com/ama/don/interior/dao/CompanyReviewDao.java
@@ -75,8 +75,9 @@ public interface CompanyReviewDao {
 
     // 리뷰를 삭제하기전 0으로 나누는 오류를 방지하기 위해
     // int countByCompanyId(Long companyId); 이 메서드로 개수가 만약 1이면
-    // adfaf 메서드 실행 아니면 adjustDeleteScoreAvg 실행
-    
+    // resetScoresIfOne 메서드 실행 아니면 adjustDeleteScoreAvg 실행
+    int resetScoresIfOne(Long companyId);
+
     // 리뷰 삭제후 점수를 다시 계산
     int adjustDeleteScoreAvg(CompanyScoreAdjustDto dto);
 
