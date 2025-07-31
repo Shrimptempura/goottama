01cec2f4d3332e0e13811b0aa3455afbe5c6853f
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-31 18:05:33 +0900

    feat(insertCompanyPost): 업체 게시글 생성

--- src/main/java/com/ama/don/interior/dto/request/CompanyPostCreateDto.java
+++ src/main/java/com/ama/don/interior/dto/request/CompanyPostCreateDto.java
@@ -19,6 +19,9 @@ public class CompanyPostCreateDto {
     private String companyPostTitle;       // 게시글 제목
 
     @NotBlank
+    private String companyPostContent;     // 게시글 내용
+
+    @NotBlank
     private String spaceType;       // 공간 종류(아파트, 주택)
     @NotBlank
     private String areaPyeong;      // 평수
@@ -26,9 +29,6 @@ public class CompanyPostCreateDto {
     private String style;           // 스타일(내추럴, 모던..)
     @NotBlank
     private String constructionDetail;   // 세부 공사(주방리모델링, 도배시공..)
-
-    @NotBlank
-    private String companyPostContent;     // 게시글 내용
 
     // 파일 관련은 다형성 file dto로 서비스에서 해결
     // private List<String> companyImgList;    // 업체가 올린 사진
