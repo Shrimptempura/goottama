e8887b640f806a7f8e3cc6c8a25e75416a77ecc4
Shrimptempura <ongtzxowns98@gmail.com>
2025-07-31 18:23:40 +0900

    wip: post poly 작성중 중간저장

---
+++ src/test/java/com/ama/don/interior/dao/CompanyPostDaoTest.java
@@ -0,0 +1,38 @@
+package com.ama.don.interior.dao;
+
+import com.ama.don.common.dao.PostDao;
+import com.ama.don.common.dto.PostDto;
+import com.ama.don.interior.dto.request.CompanyInsertDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class CompanyPostDaoTest extends AbstractCompanyTestSupport {
+
+    @Autowired
+    CompanyPostDao companyPostDao;
+
+    @Autowired
+    PostDao postDao;
+
+    @DisplayName("게시글 다형성 작성")
+    @Test
+    void insertPolyPostForCompany() {
+        CompanyInsertDto dto = insertTestCompanyWithUserLocationAndDetail();
+        Long companyId = dto.getCompanyId();
+        Long userId = dto.getUserId();
+
+        PostDto polyPost = new PostDto();
+        polyPost.setUser_id(userId);
+        polyPost.setPost_date();
+
+
+    }
+
+}
