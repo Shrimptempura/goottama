05085c25de53682b9e7d0eff0c52a2ef3d266a35
Shrimptempura <ongtzxowns98@gmail.com>
2025-08-02 21:30:58 +0900

    test(isLikedCompanyPost): 게시글 좋아요 안한 회원의 false 확인

--- src/test/java/com/ama/don/interior/dao/CompanyPostLikeDaoTest.java
+++ src/test/java/com/ama/don/interior/dao/CompanyPostLikeDaoTest.java
@@ -1,24 +1,39 @@
 package com.ama.don.interior.dao;
 
+import com.ama.don.member.dto.JoinformDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
-class CompanyPostLikeDaoTest {
+class CompanyPostLikeDaoTest extends AbstractCompanyTestSupport {
 
     @Autowired
     CompanyPostLikeDao companyPostLikeDao;
 
-    @DisplayName("회원과 게시글 좋아요 여부 확인")
+    @DisplayName("좋아요를 안누른 회원의 false 확인")
     @Test
     void isLikedCompanyPost() {
-        //
+        // 업체 생성
+        TestCompanyContext context = insertTestCompanyContext();
+        Long companyId = context.getCompanyId();
+        Long userId = context.getUser().getUserId();
+
+        // 게시글 생성
+        createCompanyPost(userId, companyId);
+
+        JoinformDto user = createTestUser("testUser111");
+
+        boolean isLiked = companyPostLikeDao.isLikedCompanyPost(companyId, user.getUserId());
+
+        // 아직 좋아요 안누름
+        assertThat(isLiked).isFalse();
     }
 
 }
