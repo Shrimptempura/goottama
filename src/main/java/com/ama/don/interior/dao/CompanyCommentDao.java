95c41b3e04f457aca33536b26df30c0c5a48d076
Shrimptempura <ongtzxowns98@gmail.com>
2025-08-03 10:12:30 +0900

    wip: 인터페이스 구조 정렬

--- src/main/java/com/ama/don/interior/dao/CompanyCommentDao.java
+++ src/main/java/com/ama/don/interior/dao/CompanyCommentDao.java
@@ -1,25 +1,39 @@
 package com.ama.don.interior.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface CompanyCommentDao {
 
     // 댓글 작성
+    int insertCompanyComment(CompanyCommentCreateDto dto);
     
     // 댓글 조회
+    List<CompanyCommentResponseDto> findCommentsByPostId(@Param("companyPostId") Long companyPostId);
 
     // 댓글 수정
+    int updateCompanyComment(CompanyCommentUpdateDto dto);
 
     // 댓글 삭제(소프트), "삭제된 댓글"으로 칸, 흐름 유지 및 신고 처리시 필요
+    int softDeleteCompanyComment(@Param("commentId") Long commentId,
+                                 @Param("userId") Long userId);
 
     // 대댓글
+    // insertCompanyComment 사용 예정
     
     // 대댓글 조회
+    // 댓글 조회에서 정렬 생각
+
     
     // 댓글 수 조회(이건 company_post로 생각중)
+    int countComentsByCompanyPostId(@Param("companyPostId") Long companyPostId);
 
     // 게시글 삭제시 게시글 내 댓글 삭제
+    // 다형성 소프트 삭제 생각
+    int softDeleteByCompanyPostId(@Param("companyPostId") Long companyPostId);
 
     // 회원관리자가 원하면 내가 작성한 댓글 모음(일단은 구현x)
 }
