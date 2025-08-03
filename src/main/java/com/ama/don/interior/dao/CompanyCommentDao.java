07abd78c2a9e86be80ce46f81455ee22290513c0
Shrimptempura <ongtzxowns98@gmail.com>
2025-08-04 04:32:45 +0900

    feat(countCommentsByTarget): 게시글 내 댓글 숫자 조회

--- src/main/java/com/ama/don/interior/dao/CompanyCommentDao.java
+++ src/main/java/com/ama/don/interior/dao/CompanyCommentDao.java
@@ -1,5 +1,6 @@
 package com.ama.don.interior.dao;
 
+import com.ama.don.common.enums.TargetType;
 import com.ama.don.interior.dto.request.CompanyCommentCreateDto;
 import com.ama.don.interior.dto.request.CompanyCommentUpdateDto;
 import com.ama.don.interior.dto.response.CompanyCommentDto;
@@ -25,25 +26,23 @@ public interface CompanyCommentDao {
     // 댓글 수정
     int updateCompanyComment(CompanyCommentUpdateDto dto);
 
-    // 댓글 삭제(소프트), "삭제된 댓글"으로 칸, 흐름 유지 및 신고 처리시 필요
+    // 댓글 삭제(소프트)
+    // 실제 쿼리 내 댓글 내용을 바꾸진 않고 is_deleted 처리만 한 후
+    // 서비스단에서 확인하여 내용 바꾸고 글씨체 등 바꿈
     int softDeleteCompanyComment(@Param("commentId") Long commentId,
                                  @Param("userId") Long userId);
 
-    // 대댓글
-    // insertCompanyComment 사용
+    // 대댓글 -> insertCompanyComment 사용
     // 대댓글은 inserCopanyComment.setParentCommentId(댓글id)로 처리
-    // 테스트 코드 완료, 쿼리는 동일
+    // 테스트 코드 완료, insertCompanyComment와 쿼리는 동일
     
     // 대댓글 조회
     // 댓글 조회에서 정렬 생각
     // findCommentsByPostId로 테스트 코드 완료, 쿼리는 동일
     
-    // 댓글 수 조회(이건 company_post로 생각중)
-    int countComentsByCompanyPostId(@Param("companyPostId") Long companyPostId);
-
-    // 게시글 삭제시 게시글 내 댓글 삭제
-    // 다형성 소프트 삭제 생각
-    int softDeleteByCompanyPostId(@Param("companyPostId") Long companyPostId);
+    // 댓글 수 조회
+    int countCommentsByTarget(@Param("targetId") Long targetId,
+                              @Param("targetType") TargetType targetType);
 
     // 회원관리자가 원하면 내가 작성한 댓글 모음(일단은 구현x)
 }
