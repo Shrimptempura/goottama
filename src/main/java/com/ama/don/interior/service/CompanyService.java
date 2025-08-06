06232a42a4c1e0b13fbdeec5450217cbd3d681d1
Shrimptempura <ongtzxowns98@gmail.com>
2025-08-06 11:14:41 +0900

    feat(CompanySereviceImpl): 업체 생성 구현

--- src/main/java/com/ama/don/interior/service/CompanyService.java
+++ src/main/java/com/ama/don/interior/service/CompanyService.java
@@ -2,6 +2,7 @@ package com.ama.don.interior.service;
 
 import com.ama.don.interior.dto.company.CompanyCreateDto;
 import com.ama.don.interior.dto.company.CompanyCreateLocationDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface CompanyService {
 
@@ -11,5 +12,6 @@ public interface CompanyService {
      * @param companyCreateDto 업체 생성 dto
      * @param locationDto 업체 주소 생성 dto
      */
-    void createCompany(Long userId, CompanyCreateDto companyCreateDto, CompanyCreateLocationDto locationDto);
+    void createCompany(Long userId, CompanyCreateDto companyCreateDto, CompanyCreateLocationDto locationDto,
+                       MultipartFile file);
 }
--- src/main/java/com/ama/don/interior/service/CompanyServiceImpl.java
+++ src/main/java/com/ama/don/interior/service/CompanyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ama.don.interior.service;
 
+import com.ama.don.common.enums.TargetType;
 import com.ama.don.interior.dao.CompanyDao;
 import com.ama.don.interior.dto.company.CompanyCreateDto;
 import com.ama.don.interior.dto.company.CompanyCreateLocationDto;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -15,12 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyDao companyDao;
+    private final FileService fileService;
 
     @Transactional
     @Override
-    public void createCompany(Long userId, CompanyCreateDto createDto, CompanyCreateLocationDto locationDto) {
-        log.info("ComapnyService - 업체 등록 시작 - userId: {}", userId);
+    public void createCompany(Long userId, CompanyCreateDto createDto, CompanyCreateLocationDto locationDto,
+                              MultipartFile file) {
 
+        log.info("ComapnyService - 업체 등록 시작 - userId: {}", userId);
         if (companyDao.isDuplicateCompanyName(createDto.getCompanyName())) {
             log.warn("CompanyService - 업체명 중복 - companyName: {}", createDto.getCompanyName());
             throw new IllegalArgumentException("이미 등록된 이름입니다.");
@@ -30,30 +34,42 @@ public class CompanyServiceImpl implements CompanyService {
         companyDao.insertCompanyDetail(createDto);
         Long companyDetailId = createDto.getCompanyDetailId();
         if (companyDetailId == null) {
-            log.error("CompanyService - DB저장 실패, companyDetailId is Null - 등록 DTO: {}", createDto);
+            log.error("CompanyService - DB저장 실패, companyDetailId is null - dto: {}", createDto);
             throw new IllegalStateException("companyDetailId에 값이 안들어옴");
         }
 
+        // location 테이블 생성
         companyDao.insertLocation(locationDto);
         Long locationId = locationDto.getLocationId();
         if (locationId == null) {
-            log.error("CompanyService - DB저장 실패, locationId is Null - 등록 DTO: {}", locationDto);
+            log.error("CompanyService - DB저장 실패, locationId is null - dto: {}", locationDto);
             throw new IllegalStateException("locationId에 값이 안들어옴");
         }
 
-        // company 테이블의 fk값 더하기, userId, companyDetailId, locationId
+        // company 테이블의 fk값: userId, companyDetailId, locationId
         CompanyInsertDto insertDto = new CompanyInsertDto();
         insertDto.setUserId(userId);
         insertDto.setCompanyDetailId(companyDetailId);
         insertDto.setLocationId(locationId);
 
+        // 업체 생성
         companyDao.insertCompany(insertDto);
 
-        // file
         Long companyId = insertDto.getCompanyId();
-        // companyId null 확인
+        if (companyId == null) {
+            log.error("CompanyService - 업체 등록 실패 - companyId is null");
+            throw new IllegalStateException("companyId가 없음, 업체 등록 실패");
+        }
 
+        if (file == null || file.isEmpty()) {
+            log.warn("CompanyService - 업체 이미지 필요 - companyId: {}", companyId);
+            throw new IllegalArgumentException("1장의 이미지는 필수");
+        }
 
+        // 이미지 저장
+        fileService.saveFile(userId, TargetType.INTERIOR, companyId, file);
+        log.info("CompanyService - 업체 이미지 저장 성공 - companyId: {}", companyId);
 
+        log.info("CompanyService - 업체 등록 성공 - companyId: {}", companyId);
     }
 }
