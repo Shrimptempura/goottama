e6d359ebc0c219b268c77c05ef8d096c898e6f6d
Shrimptempura <ongtzxowns98@gmail.com>
2025-08-07 22:42:17 +0900

    refactor(CompanyDao): 책임에 따라 분리 및 주석 시각화

--- src/main/java/com/ama/don/interior/dao/CompanyDao.java
+++ src/main/java/com/ama/don/interior/dao/CompanyDao.java
@@ -15,52 +15,69 @@ import java.util.Optional;
 @Mapper
 public interface CompanyDao {
 
+    // === create ===============================================
+    /**
+     * [업체 등록 흐름]
+     * 1. 업체 이름 중복 검사 (isDuplicateCompanyName)
+     * 2. 업체 상세 정보 등록 (company_detail)              -> insertCompanyDetail()
+     * 3. 업체 위치 등록 (location)                         -> insertLocation()
+     *      - 일정 변경으로 거의 사용안함 생성은 필수
+     * 4. 업체 테이블 생성 (company)                        -> insertCompany()
+     *      - 외래키: userId, companyDetailId, locationId
+     * 5. 이미지 1장 업로드 필수 (FileService)
+     * - 업체 생성 허락은 관리자 또는 바로 생성, 권한이 바뀜 100 -> 200
+     */
+    // 업체 상세 정보 등록 (company_detail)
+    void insertCompanyDetail(CompanyCreateDto dto);
+
+    // 위치 위치 정보 등록 (location)
+    void insertLocation(CompanyCreateLocationDto dto);
+
+    // 업체 생성 (company)
+    void insertCompany(CompanyInsertDto dto);
+
+    // 업체 이름 중복 검사
+    Boolean isDuplicateCompanyName(@Param("companyName") String companyName);
+    
+    // 업체 id로 업체 이름 조회
+    String getCompanyNameById(@Param("companyId") Long companyId);
+
+    // === read ==================================================
+    // 홈에서 보는 업체 리스트 -> 불안정한 추후 확인 필요
+    List<CompanyHomeDto> findCompanyHomeList();
+
+    // 업체 상세 페이지에서 보는 정보 탭
+    CompanyDetailDto selectDetailCompany(@Param("companyId") Long companyId);
+
+    // 업체 상세 페이지에서 보는 좌측 따라다니는 요약 정보 박스
+    // 박스 안에 별점과 관련된 처리 필요
+    CompanySummaryDto selectSummaryCompany(@Param("companyId") Long companyId);
+
+    // 업체 상세페이지의 photos 탭, 사진 보여주기
+    List<FileDto> selectCompanyPhoto(@Param("targetType") String targetType,
+                                     @Param("targetId") Long targetId);
+
+    // 회원아이디로 업체아이디 찾기
+    Optional<Long> findCompanyIdByUserId(@Param("userId") Long userId);
+
+    // === update ==================================================
+    // 업체 정보 수정 (company_detail + file)
+    int updateCompanyDetail(CompanyUpdateDto dto);
+
+    // === delete ==================================================
+    // 업체 탈퇴, 소프트 삭제 (company, is_deleted)
+    int deleteCompany(@Param("companyId") Long companyId);
+
+
+
+
+    // === utils ==================================================
     // 회원 생성 예시
     void insertUser(JoinformDto dto);
 
-    // 회원아이디로 업체아이디 찾기, 회원아이디로 업체 아이디가 없으면 optional
-    Optional<Long> findCompanyIdByUserId(@Param("userId") Long userId);
-
-    // 업체 정보 등록 + 내부적으로 정보 등록하면서 권한등급(번호)를 회원->업체 변경
-    // dto가 테이블 2개의 내용이므로, 먼저 company_detail 테이블 먼저 생성
-    void insertCompanyDetail(CompanyCreateDto dto);
-
-    // 업체 이름 중복 검사, insertCompanydetail에서 확인해야함
-    Boolean isDuplicateCompanyName(@Param("companyName") String companyName);
-
-    // 위치 정보 등록
-    void insertLocation(CompanyCreateLocationDto dto);
-
-    // company 테이블, pk: user_id, company_detail, location_id
-    void insertCompany(CompanyInsertDto dto);
-
-    // homeDto dao, mapper 누락
-    List<CompanyHomeDto> findCompanyHomeList();
-
-    // 업체 상세 정보 조회
-    CompanyDetailDto selectDetailCompany(@Param("companyId") Long companyId);
-
-    // 업체 요약 정보 조회(박스)
-    // company_detail, company_score_avg
-    CompanySummaryDto selectSummaryCompany(@Param("companyId") Long companyId);
-
-    // 업체 정보 수정(detail + company + location(api->service)), file
-    // company_detail table 수정(대부분)
-    int updateCompanyDetail(CompanyUpdateDto dto);
-
-    // 이미지는 다형성 file 사용
-
-    // 업체 상세페이지의 photos 탭 사진 보여주기
-    List<FileDto> selectCompanyPhoto(@Param("targetType") String targetType,
-                                   @Param("targetId") Long targetId);
-
-
-    // 업체의 주소가 바뀌면 company_addr를 가져다 위치 정보 업데이트
-    // 현재 사용 안함
+    // === reserved ==================================================
+    // 업체의 주소가 바뀌면 companyDetail의 companyAddr를 위도경도로 api 사용하여 업데이트
+    // 현재 사용 안함 향후 일정 봐서 사용
     int updateLocation(CompanyUpdateLocationDto dto);
-
-    // 업체 탈퇴(is_deleted), company table
-    int deleteCompany(@Param("companyId") Long companyId);
-
-    // api location -> 정렬로 대체
+    
 }
