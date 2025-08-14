633c2ae5f43de2eb263151e57017d86054a55dc9
EUM11327 <gustjd11327@gmail.com>
2025-08-14 17:02:35 +0900

    ooo

---
+++ src/main/java/com/ama/don/community/dto/CommunityPostListDto.java
@@ -0,0 +1,23 @@
+
+package com.ama.don.community.dto;
+
+import java.sql.Timestamp;
+import com.ama.don.common.enums.TargetType;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunityPostListDto {
+	private Long post_id;
+	private String post_title;
+	private String post_content;
+	private Timestamp post_date;
+	private int view_count;
+	private int like_count;
+	private int comment_count;
+
+	private Long targetId;
+	private TargetType targetType;
+}
